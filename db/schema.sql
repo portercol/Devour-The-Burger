56dfa9f2655e525849dbcebf5ab738d27f8de574
Collin Porter <colls.potta@gmail.com>
2020-10-18 20:00:17 -0600

    set up code to pre-populate database

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR (30) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
