95046bb1f9edd371ec746920f2d7fcd890cfd7ef
Collin Porter <colls.potta@gmail.com>
2020-10-18 21:34:17 -0600

    create get, post and put routes for respective burger info

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,45 @@
 var express = require("express");
 
 // Import burger.js file
-var burger = require("../burger.js")
+var burger = require("../models/burger.js");
+
+// Create instance of express router
+var router = express.Router();
+
+// Create get route to get all burgers
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+// Create post route for create burger
+router.post("/api/burgers", (req, res) => {
+    burger.create(["name", "bbq"], [req.body.name, req.body.bbq], (result) => {
+        res.json({ id: result.insertID });
+    });
+});
+
+// Create put route to get burgers id
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.update(
+        {
+            bbq: req.body.bbq
+        },
+        condition,
+        (result) => {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
