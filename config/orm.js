776ce31cb06b544effb41a01fb7f185294c153c5
Collin Porter <colls.potta@gmail.com>
2020-10-18 21:37:50 -0600

    created object with functions for all SQL statements and exported out

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,57 @@
+// Import connection.js file
 var connection = require("./connection.js");
+
+// Set up orm object with functions 'select all, insert one and update one' SQL statements
+var orm = {
+
+    selectAll: (table, cb) => {
+        const queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    insertOne: (table, cols, vals, cb) => {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    updateOne: (table, objColVals, condition, cb) => {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+};
 
 module.exports = orm;
