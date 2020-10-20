69e04e2133d77ccc436604912ff7bbfc561ea35e
Collin Porter <colls.potta@gmail.com>
2020-10-19 21:21:25 -0600

    moved controller import to top to flow better with the rest of the imports

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 8000;
 // Create express server
 const app = express();
 
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -19,9 +22,6 @@ const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
