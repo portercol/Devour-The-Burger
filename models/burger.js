f7822781b90533f3004cd7c05613616d15cb0399
Collin Porter <colls.potta@gmail.com>
2020-10-19 21:20:31 -0600

    changed vars to consts, added ES6 syntax

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,24 @@
-var orm = require("../orm.js");
+// Import the ORM to create functions that interact with DB
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: (cb) => {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+            // console.log(selectAll);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: (cols, vals, cb) => {
+        orm.createOne("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+};
 
 module.exports = burger;
