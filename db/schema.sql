1758a278bd0855c4a1aa5f4c1b8d01f34b3c7590
Collin Porter <colls.potta@gmail.com>
2020-10-21 10:20:49 -0600

    set default to 0

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR (30) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN DEFAULT 0,
     PRIMARY KEY (id)
 );
