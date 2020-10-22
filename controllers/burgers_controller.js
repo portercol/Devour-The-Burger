cfcef10b18f8f7e5b5fc13a7e129d03a7c6cca4b
Collin Porter <colls.potta@gmail.com>
2020-10-21 22:42:27 -0600

    formatted doc and removed unused console logs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,7 +19,6 @@ router.get("/", (req, res) => {
 
 // Create post route for create burger
 router.post("/api/burgers", (req, res) => {
-    console.log("Is this being hit?", req.body);
     burger.create(req.body.burger_name, (result) => {
         res.json({ id: result.insertID });
     });
@@ -29,14 +28,13 @@ router.post("/api/burgers", (req, res) => {
 router.put("/api/burgers/:id", (req, res) => {
     var condition = req.params.id;
     // console.log("condition", condition);
-    console.log("hit controller.js");
     burger.update(req.body.devoured, condition, (result) => {
         if (result.changedRows === 0) {
             // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
-        res.status(200).end();
-    }
+            res.status(200).end();
+        }
     });
 });
 
