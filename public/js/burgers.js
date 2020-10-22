379ce2dcccea6af340a7577d9bcefb365c2af903
Collin Porter <colls.potta@gmail.com>
2020-10-21 22:48:54 -0600

    formatted doc, added documents, changed variable names for add and devour burger, fixed the PUT ajax request path

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,45 +1,39 @@
-// Grab the form
-$('#form').on('submit', function(event){
-    // console.log("is it hit?");
+// Create button 'on click' for the Devour button
+$('#submitBurger').on('click', function (event) {
     // Prevent the default action of page reloading
     event.preventDefault();
-  
-    // create a new burger
-    let newBurger = {
-      // set the burger name to the value the user types in
-      burger_name: $('#addBurger').val().trim()
+
+    // For adding a burger - set the name to the value of the input
+    // Store in a variable
+    let addBurger = {
+        burger_name: $('#addBurger').val().trim()
     }
-    console.log(newBurger);
-  
-    // Send Ajax post request - set the data to the newBuger 
+
+    // Send Ajax post request - set the data to the add burger 
     $.ajax('/api/burgers', {
-      method: 'POST',
-      data: newBurger
-    }).then(function(){
-      // reloads the page after a post 
-      location.reload();
+        method: 'POST',
+        data: addBurger
+    }).then(function () {
+        location.reload();
     })
-  
-  });
-  
-  $('#devourBurger').on('click', function(){
-    // using the data method to update data that has a 'burgerId' value
+
+});
+
+$('#devourBurger').on('click', function () {
+    // Storing ID value and devoured value in variables
     const id = $(this).data('id');
-    const devoured = $(this).data('devoured')
-    console.log(id)
-  
-    // create a new burger
-    let changeBurger = {
-      // set the burger name to the value the user types in
-      devoured: devoured
+    const devoured = 1;
+
+    // Storing the property of 'devoured' to the variable which is === 1 in a variable
+    let devourBurger = {
+        devoured: devoured
     }
-  
+
     // Send Ajax put request to update the data 
-    $.ajax("/:id", {
-      method: 'PUT',
-      data: changeBurger
-    }).then(function(){
-      location.reload();
+    $.ajax("/api/burgers/" + id, {
+        method: 'PUT',
+        data: devourBurger
+    }).then(function () {
+        location.reload();
     })
-  
-  });
+});
