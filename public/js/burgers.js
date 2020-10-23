9af35b2f06467ceb23c7780b5d18c6afa2e1f9c3
Collin Porter <colls.potta@gmail.com>
2020-10-22 23:48:32 -0600

    changed button to match class from handlebars

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -19,11 +19,11 @@ $('#submitBurger').on('click', function (event) {
 
 });
 
-$('#devourBurger').on('click', function () {
+$('.devourBurger').on('click', function () {
     // Storing ID value and devoured value in variables
     const id = $(this).data('id');
     const devoured = 1;
-
+    console.log(id);
     // Storing the property of 'devoured' to the variable which is === 1 in a variable
     let devourBurger = {
         devoured: devoured
